62a698f36d145cfa71e9efa28c2595fc4421137a
liaojia <2380153794@qq.com>
2019-12-24 10:20:55 +0800

    正在处理被删除路由显示的异常路由表问题

--- ConsoleApplication1/Deletevertex.cpp
+++ ConsoleApplication1/Deletevertex.cpp
@@ -6,5 +6,6 @@ void Deletevertex()
 	printf("请输入你要删除的是第几个顶点v\n");
 	scanf("%d", &i);
 	DeleteVertex(&g1, i);
+    isExist[i] = 0;
 	menu();
 }
--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -12,6 +12,7 @@
 AdjMGraph g1;
 int a[MaxSize];
 RowColWeight rcw[100];
+int isExist[10];
 
 
 
@@ -22,41 +23,45 @@ void putList()
 	printf("请输入你要生成的路由表的起始路由器是第几个路由器\n");
 	scanf("%d", &xxx);
 	xxx--;
-	Dijkstra(&g1, xxx, distance, path);
-	FILE *fp;
-	printf("目的路由  下一跳路由\n");
-	fp = fopen("luyoubiao.txt", "w");
-	for (int i = 0; i < g1.vertices.size; i++)
-	{
-		if (i == xxx) {
-			printf("%5d%12d\n", xxx + 1, xxx + 1);
-			continue;
-		}
-		j = i;
-		while (path[j] != xxx)
-		{
-			j = path[j];
-			if (j == -1) break;
-		}
-		printf("%5d%12d\n", i + 1, j + 1);
-		fprintf(fp, "%5d%12d\n", i + 1, j + 1);
-	}
-	fclose(fp);
-	printf("从顶点%d到其他各顶点的最短距离为：\n", a[xxx]);
-	for (j = 0; j < g1.vertices.size; j++) {
-		bool flag = 1;
-		if (j == xxx) {
-			printf("到顶点%d的最短距离为%d\n", a[j], distance[j]);
-			continue;
-		}
-		//if (j == xxx) continue;
-		for (int i = 0; i < g1.vertices.size; ++i) {
-			if (g1.edge[j][i] != MaxWeight)   flag = 0;
-		}
-		if (flag == 1)  continue;
-		printf("到顶点%d的最短距离为%d\n", a[j], distance[j]);
-	}
-	menu();
+    Dijkstra(&g1, xxx, distance, path);
+    if(isExist[xxx] == 0){
+        printf("无此路由表\n");
+    }else{
+        FILE *fp;
+        printf("目的路由  下一跳路由\n");
+        fp = fopen("luyoubiao.txt", "w");
+        for (int i = 0; i < g1.vertices.size; i++)
+        {
+            if (i == xxx) {
+                printf("%5d%12d\n", xxx + 1, xxx + 1);
+                continue;
+            }
+            j = i;
+            while (path[j] != xxx)
+            {
+                j = path[j];
+                if (j == -1) break;
+            }
+            printf("%5d%12d\n", i + 1, j + 1);
+            fprintf(fp, "%5d%12d\n", i + 1, j + 1);
+        }
+        fclose(fp);
+        printf("从顶点%d到其他各顶点的最短距离为：\n", a[xxx]);
+        for (j = 0; j < g1.vertices.size; j++) {
+            bool flag = 1;
+            if (j == xxx) {
+                printf("到顶点%d的最短距离为%d\n", a[j], distance[j]);
+                continue;
+            }
+            //if (j == xxx) continue;
+            for (int i = 0; i < g1.vertices.size; ++i) {
+                if (g1.edge[j][i] != MaxWeight)   flag = 0;
+            }
+            if (flag == 1)  continue;
+            printf("到顶点%d的最短距离为%d\n", a[j], distance[j]);
+        }
+    }
+    menu();
 
 }
 
@@ -69,27 +74,28 @@ void putList()
 
 int main()
 {
-	//	AdjMGraph g1;
-	int i, j, k;
-	FILE *fp;           //读入文件
-//	int a[MaxSize];
-  //  RowColWeight rcw[100];
-	fp = fopen("tuoputu10.txt","r");
+    malloc(a,1,10*sizeof(int));
+    //	AdjMGraph g1;
+    int i, j, k;
+    FILE *fp;           //读入文件
+    //	int a[MaxSize];
+    //  RowColWeight rcw[100];
+    fp = fopen("tuoputu10.txt","r");
 
-	fscanf(fp, "%d", &i);
-	for (k = 0; k < i; k++)
-	{
-		fscanf(fp, "%d", &a[k]);
-	}
-	fscanf(fp, "%d", &j);
-	for (k = 0; k < j; k++)
-	{
-		fscanf(fp, "%d%d%d", &rcw[k].row, &rcw[k].col, &rcw[k].weight);
-	}
-	fclose(fp);
-	GraphInitiate(&g1);
-	CreatGraph(&g1, a, i, rcw, j);
-	menu();
+    fscanf(fp, "%d", &i);
+    for (k = 0; k < i; k++)
+    {
+        fscanf(fp, "%d", &a[k]);
+    }
+    fscanf(fp, "%d", &j);
+    for (k = 0; k < j; k++)
+    {
+        fscanf(fp, "%d%d%d", &rcw[k].row, &rcw[k].col, &rcw[k].weight);
+    }
+    fclose(fp);
+    GraphInitiate(&g1);
+    CreatGraph(&g1, a, i, rcw, j);
+    menu();
 
 }
 
